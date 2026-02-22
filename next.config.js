74fd433a5c6e2174e1df0e84c54209aba0846358
Kelvdra <145371709+Kelvdra@users.noreply.github.com>
2026-02-22 21:37:34 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  serverExternalPackages: ["@napi-rs/canvas"],
+};
+
 module.exports = nextConfig;
